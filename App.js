08aa96bb7a26a4cc8766bf7677fb215e5cd1e4e4
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2023-07-18 07:41:29 +0530

    JSX

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-const heading = React.createElement("div", { className: "div" }, [
-  React.createElement("div", {}, [
-    React.createElement("h1", { id: "header" }, "I am from React Div!"),
-    React.createElement("h1", { id: "header" }, "I am from React Div!"),
-  ]),
-  React.createElement("div", {}, [
-    React.createElement("h1", { id: "header" }, "I am from React Div!"),
-    React.createElement("h1", { id: "header" }, "I am from React Div!"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element
+
+// const heading = [
+//   React.createElement("h1", { className: "heading" }, "Namaste React!"),
+//   React.createElement("div", { className: "heading" }, [
+//     React.createElement("h1", {}, "Hii Jainuddin"),
+//     React.createElement("h1", {}, "Hello jeee"),
+//   ]),
+// ];
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// JSX--HTML like or XML like syntax but not HTML.
+
+const jsxheading = <h1>Namaste React By JSX!</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxheading);
