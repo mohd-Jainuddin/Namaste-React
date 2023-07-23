3e63e69a93c1c2786717301f4e954c194f3a9f11
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2023-07-23 22:32:14 +0530

    Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,34 @@
 import ReasturantCards from "./ResturantCards";
-import resObj from "../utills/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [resList,setResList] = useState(resObj);
-  return (
+  const [resList, setResList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.846693699999996&lng=80.94616599999998&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    setResList(
+      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
+  return resList.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            const resObjs = resObj.filter((res) => res.avgRating > 4.3);
+            const resObjs = resList.filter((res) => res.info.avgRating >= 4);
             setResList(resObjs);
           }}
         >
@@ -19,7 +37,7 @@ const Body = () => {
       </div>
       <div className="resturent-list">
         {resList.map((res) => (
-          <ReasturantCards resData={res} key={res.id} />
+          <ReasturantCards resData={res.info} key={res.info.id} />
         ))}
       </div>
     </div>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,20 @@
+const Shimmer = () => {
+    return (
+      <div className="shimmerContainer">
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+        <div className="fake-card"></div>
+      </div>
+    );
+}
+
+export default Shimmer;
